ba64c289d1fa42c353f9446a07b738c38e661605
lehongminhh2004 <lehongminhfw1@gmail.com>
2024-04-25 10:39:47 +0700

    admin

---
+++ admin/cartegoryadd.php
@@ -0,0 +1,12 @@
+<div class="admin-content-right">
+            <div class="admin-content-right-cartegory_add">
+                <h1>them danh muc</h1>
+                <form action="" method="post">
+                    <input type="text" placeholder="nhap ten danh muc">
+                    <button type="submit">them</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ admin/slider.php
@@ -0,0 +1,24 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="#">danh muc</a>
+                    <ul>
+                        <li><a href="#">them danh muc</a></li>
+                        <li><a href="#">danh sach danh muc</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">loai sp</a>
+                    <ul>
+                        <li><a href="#">them loai sp</a></li>
+                        <li><a href="#">danh sach laoisp</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">san pham</a>
+                    <ul>
+                        <li><a href="#">them danh sp</a></li>
+                        <li><a href="#">danh sach sp</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
